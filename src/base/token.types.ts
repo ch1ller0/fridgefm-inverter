03c8d5ccda5c54f1bbcffae383c84473d8615e6d
Grigory <g.gorshkov@tinkoff.ru>
2022-05-03 00:16:39 +0400

    Fixed container.get interface (#35)

--- src/base/token.types.ts
+++ src/base/token.types.ts
@@ -4,17 +4,13 @@ import type { TodoAny } from './util.types';
  */
 export type Token<T> = { symbol: symbol; type?: T; optionalValue?: T; multi?: true };
 /**
- * Regular token provided value type
- */
-export type TokenProvide<T> = T extends Token<infer A> ? A : never;
-/**
  * Token declaration type. This is the interface tokens are declared in inject field
  */
 export type TokenDec<T> = T | { token: T; optional: true };
 /**
  * Token declaration provided value type. It takes into account the optionality and multiness of a token
  */
-export type TokenDecProvide<T> = T extends TokenDec<infer A> & { optional: true }
+export type TokenProvide<T> = T extends TokenDec<infer A> & { optional: true }
   ? A extends Token<infer B> & { multi: true }
     ? B[]
     : A extends Token<infer B>
@@ -35,5 +31,5 @@ export type TokenDecTuple = readonly [...ReadonlyArray<TokenDec<Token<TodoAny>>>
  * Tuple of token declaration provided value types
  */
 export type TokensDeclarationProvide<DepToks extends TokenDecTuple> = {
-  +readonly [Index in keyof DepToks]: TokenDecProvide<DepToks[Index]>;
+  +readonly [Index in keyof DepToks]: TokenProvide<DepToks[Index]>;
 };

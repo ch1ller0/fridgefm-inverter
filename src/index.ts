4c3fa7e10e5416be6d9e0907cd1d8449b4b2b6ac
Горшков Григорий Александрович <g.gorshkov@tinkoff.ru>
2022-05-03 00:40:26 +0400

    add

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export { injectable } from './module/provider-declaration';
 export { declareModule } from './module/module-declaration';
 export { createToken, modifyToken } from './base/token';
 // export only types that might be used by external code
-export type { TokenProvide, TokensDeclarationProvide } from './base/token.types';
+export type { TokenProvide, TokenDecProvide, TokensDeclarationProvide } from './base/token.types';
 export type { ContainerConfiguration } from './module/container.types';
 export type { ModuleConfig } from './module/module.types';
 export type { ProviderConfig } from './module/provider.types';

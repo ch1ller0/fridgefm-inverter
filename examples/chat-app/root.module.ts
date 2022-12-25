9b1fc61781cb5d872be53356535d44bd904e01b6
Grigory <petralmazov100@gmail.com>
2022-12-25 17:57:29 +0400

    Fix chat example (#44)

--- examples/chat-app/root.module.ts
+++ examples/chat-app/root.module.ts
@@ -1,50 +1,33 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import net from 'net';
 import { createModule, injectable, createToken } from '../../src/index';
 import { ServerModule } from './server.module';
 import { ClientModule } from './client.module';
 
 const { SERVER_INIT } = ServerModule.exports;
-const { CLIENT_INIT, PORT } = ClientModule.exports;
+const { CLIENT_INIT, REMOTE_HOST } = ClientModule.exports;
 const ROOT = createToken<void>('root');
-const IS_PORT_BUSY = createToken<(port: number) => Promise<boolean>>('root:is-port-busy');
 
 export const RootModule = createModule({
   name: 'RootModule',
   providers: [
     injectable({
-      provide: IS_PORT_BUSY,
-      useValue: (port) =>
-        new Promise((resolve, reject) => {
-          const tester = net
-            .createServer()
-            .once('error', function (err) {
-              // @ts-ignore
-              if (err.code != 'EADDRINUSE') return reject(err);
-              resolve(true);
-            })
-            .once('listening', function () {
-              tester
-                .once('close', function () {
-                  resolve(false);
-                })
-                .close();
-            })
-            .listen(port);
-        }),
-    }),
-    injectable({
       provide: ROOT,
-      inject: [IS_PORT_BUSY, PORT, SERVER_INIT, CLIENT_INIT] as const,
-      useFactory: async (isPortBusy, port, initServer, initClient) => {
-        const isBusy = await isPortBusy(port);
-        (isBusy ? initClient : initServer)();
+      inject: [SERVER_INIT, CLIENT_INIT, { token: REMOTE_HOST, optional: true }] as const,
+      useFactory: (initServer, initClient, remoteHost) => {
+        if (typeof remoteHost !== 'undefined') {
+          // if remote host is set, we start the client immediately
+          return initClient();
+        }
+        return initServer().catch((e) => {
+          if (e.code === 'EADDRINUSE') {
+            // that means that the port is already taken, which means the server is already running
+            // and we can run the client
+            return initClient();
+          }
+          throw e;
+        });
       },
     }),
   ],
-  extend: {
-    // if you want to configure port via dynamic module
-    forRoot: ({ port }: { port: number }) => [injectable({ provide: PORT, useValue: port })],
-  },
   exports: { ROOT },
 });
